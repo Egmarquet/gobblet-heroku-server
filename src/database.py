95acd54f359a30d99e333f22ce2cf75594f0d3d9
egmarquet <egmarquet@gmail.com>
2020-05-10 10:48:38 -0400

    Setup socket communication

---
+++ src/database.py
@@ -0,0 +1,252 @@
+import os, uuid, base64, json
+import psycopg2
+import config_globals
+import datetime
+DB_PATH = config_globals.DB_PATH
+
+def add_user(userID):
+    conn = None
+    curr = None
+
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+        "INSERT INTO users (userid, created_on) VALUES (%s, %s)",
+        (userID, datetime.datetime.now(),)
+        )
+        conn.commit()
+
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+
+def delete_user(userID):
+    """
+    Deleting a user from databse on disconnect
+    On disconnect:
+
+    Delete users from users
+    If in active games, delete the user from that lobby
+    If both users are deleted from active games, delete the
+    active game lobby
+    """
+    conn = None
+    curr = None
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+            """
+            DELETE FROM users WHERE userid = %s;
+
+            UPDATE active_games SET p1 = NULL
+            WHERE p1 = %s;
+
+            UPDATE active_games SET p2 = NULL
+            WHERE p2 = %s;
+
+            DELETE FROM active_games
+            WHERE p1 IS NULL and p2 IS NULL;
+            """,
+            (userID, userID, userID,)
+        )
+        conn.commit()
+
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+
+def get_user(userID):
+    conn = None
+    curr = None
+    output = None
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+        """
+        SELECT *
+        FROM users
+        WHERE userid = %s
+        """,
+        (userID,)
+        )
+        output = curr.fetchone()
+        if output:
+            output = output[0]
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+        return output
+
+def add_room(roomID, p1ID, p2ID=None):
+    conn = None
+    curr = None
+
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+        "INSERT INTO active_games (roomid, p1, p2) VALUES (%s, %s, %s)",
+        (roomID, p1ID, p2ID)
+        )
+        conn.commit()
+
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+
+def add_user_to_room(roomID, userID, player="p2"):
+    conn = None
+    curr = None
+
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+
+        query = f"""
+        UPDATE active_games SET {player} = %s
+        WHERE roomid = %s
+        """
+        curr.execute(query, (userID, roomID,))
+        conn.commit()
+
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+
+def user_in_room(userID):
+    """
+    Returns: Room tuple (roomID, p1, p2) if found, null otherwise
+    """
+    conn = None
+    curr = None
+    output = None
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+        """
+        SELECT *
+        FROM active_games
+        WHERE p1 = %s OR p2 = %s
+        """,
+        (userID,userID,)
+        )
+
+        query_result = curr.fetchone()
+        if query_result:
+            output = query_result
+
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+        return output
+
+def delete_room(roomID):
+    conn = None
+    curr = None
+
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+        "DELETE FROM active_games WHERE roomid = %s",
+        (roomID,)
+        )
+        conn.commit()
+
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+
+def get_room(roomID):
+    """
+    Returns:
+        None if no entry is found
+    else tuple: (roomID, p1, p2)
+    """
+    conn = None
+    curr = None
+    output = None
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+        """
+        SELECT *
+        FROM active_games
+        WHERE roomid = %s
+        """,
+        (roomID,)
+        )
+        output = curr.fetchone()
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+        return output
+
+def get_all_users():
+    conn = None
+    curr = None
+    output = None
+    try:
+        conn = psycopg2.connect(DB_PATH)
+        curr = conn.cursor()
+        curr.execute(
+        """
+        SELECT *
+        FROM users
+        """
+        )
+        output = curr.fetchall()
+    except Exception as e:
+        raise
+
+    finally:
+        if curr:
+            curr.close()
+        if conn:
+            conn.close()
+        return output
---
+++ src/server.py
@@ -0,0 +1,124 @@
+from flask import Flask, request, session
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+from flask_cors import CORS, cross_origin
+import config_globals
+import database
+import uuid, base64, json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = config_globals.SECRET_KEY
+socketio = SocketIO(app, cors_allowed_origins="*")
+app.debug = True
+
+"""
+Current status of lobby and gamestate:
+status:
+    "dne": If the lobby attempting to join does not exist
+    "full": If the lobby attempting to join is full
+    "active": If the lobby is active with 2 players tracking state
+    "waiting": If the lobby is waiting for players
+    "disconnected": If a player in the lobby disconnected
+
+roomID: roomID
+p1ID: p1ID
+p2ID: p2ID
+"""
+def lobby_status(status, roomID, p1ID, p2ID):
+    obj = {
+        "status": status,
+        "roomID": roomID,
+        "p1ID": p1ID,
+        "p2ID": p2ID,
+    }
+    return obj
+
+@socketio.on('connect')
+def connect():
+    sid = request.sid
+    database.add_user(sid)
+    emit("connection_accepted", sid)
+
+@socketio.on('disconnect')
+def disconnect():
+    """
+    Clean up database on disconnect, destroy active_game lobbies
+    if disconnecting player is the last player in the lobby
+
+    Emits "disconnect" status to prompt other player in lobby to
+    also disconnect from the socket
+    """
+    print("deleting user")
+    sid = request.sid
+    room_data = database.user_in_room(sid)
+    if room_data:
+        payload = lobby_status("disconnected",room_data[0],None,None)
+        emit('lobby_status', payload, broadcast=True)
+        leave_room(room_data[0])
+
+    database.delete_user(sid)
+
+@socketio.on('create_lobby')
+def create_lobby():
+    """
+    Returns:
+        lobby_status object
+    """
+    sid = request.sid
+    room_data = database.user_in_room(sid)
+    if not room_data:
+        roomID = base64.urlsafe_b64encode(uuid.uuid1().bytes).rstrip(b'=').decode('ascii')[:6]
+        database.add_room(roomID, sid)
+        join_room(roomID)
+        payload = lobby_status("waiting", roomID, sid, None)
+        emit("lobby_status", payload, room=roomID)
+
+@socketio.on('join_lobby')
+def join_lobby(data):
+    """
+    Args:
+        data (json): {"roomID": ID of room}
+
+    Returns:
+        Emits a lobby_status depending on the room state
+    """
+    sid2 = request.sid
+    roomID = data['roomID']
+    room_data = database.get_room(roomID)
+    if room_data:
+        roomID, p1, p2 = room_data
+        # lobby is full
+        if p1 and p2:
+            print("Attempting to join fully lobby")
+            payload = lobby_status("full", roomID, None, None)
+            emit("lobby_status", payload)
+
+        # lobby needs player 2
+        if not p2 and p1:
+            database.add_user_to_room(roomID, sid2)
+            join_room(roomID)
+            payload = lobby_status("active", roomID, p1, sid2)
+            emit("lobby_status", payload, room=roomID, broadcast=True)
+
+    else:
+        # lobby does not exist
+        payload = lobby_status("dne", roomID, None, None)
+        emit("lobby_status", payload)
+
+@socketio.on('make_move')
+def make_move(data):
+    """
+    Args:
+        data (json):
+        {
+            "roomID": roomid
+            "move": [] coords
+            "sender": "p1" or "p2"
+        }
+
+    Returns:
+        Emits on socket 'get_move' the move and sender
+    """
+    emit('get_move', data, room=data['roomID'])
+
+if __name__ == '__main__':
+    socketio.run(app)
